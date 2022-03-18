06e472eb290379f1b19d0c7fd18268abab5c4991
Ester The Otter <83263521+EsterTheOtter@users.noreply.github.com>
2022-03-18 06:21:31 +0000

    Write basic rundown getting into Objects

---
+++ index.js
@@ -0,0 +1,26 @@
+// Objects
+
+/*Disambiguation
+
+container: that which holds something with substance, NOT to be confused with the substance itself
+-Much in the same way that with a glass of water, you do not confuse the glass with the water and vice versa
+
+value: the substance/data
+-As a book is to words, arithmetic is to plus/minus, the relationship between a value and container is interdependant
+
+property: a value
+
+
+
+terminology continued-
+variable: a container for values/data
+object: a container that holds multiple values
+-Objects have properties(values) and actions(methods)
+-Objects pretty much self-contained variable/function duos?
+
+let x= 'Hello'
+let y= 'Bonjour'
+console.log(y)
+The above: var x is called to be displayed onto the console. It's 'property' or 'value' is then presented
+
+*/
