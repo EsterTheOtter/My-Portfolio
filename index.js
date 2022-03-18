91d27b14602f3c355f6bd1b5d7437a8a44ac9622
Ester The Otter <83263521+EsterTheOtter@users.noreply.github.com>
2022-03-18 06:46:46 +0000

    Further progress in project/notes, commit with proper conventions in mind

--- index.js
+++ index.js
@@ -23,4 +23,28 @@ let y= 'Bonjour'
 console.log(y)
 The above: var x is called to be displayed onto the console. Its 'property' or 'value' is then presented
 
+The following is a representation of an object:
+*/
+let x= {
+afternoon: '12:00',
+evening: '17:00'
+}
+//The above is in 'Object-literal' convention. Within the object (just between the '{}'), we have the properties in a key-value pair (afternoon being the key, the string '12:00' being the value)
+console.log(x.evening)
+//Already we have differences between an object and a variable/function.
+/*
+  -between properties we have commas acting as separators
+  -in a key-value pair, unlike a variable-value convention, instead of an '=' assignment operator, we have a colon ':'
+  -no declaration of a variable type within the object, assuming object is the universal baseline replacement for said declaration? Following code takes the mantle so to speak?
+      -The statements...
+      let x= 'hello'
+      let y= 'world'
+      console.log(x + ' ' + y)
+      prints 'hello world'
+      are equivalent to...
+      let x= {
+        rst: 'hello',
+        nd: 'world',
+      }
+      console.log(x.)
 */
